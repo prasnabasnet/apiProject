ffcf5c1d6a1857abd616534a057bbec853930d7e
Prasna Basnet <prasnabasnet18@gmail.com>
2024-12-09 16:45:02 +0545

    initial commit

---
+++ map/README.md
@@ -0,0 +1,11 @@
+# MAP
+
+```
+    Create Virtual Environment:
+    1. python -m venv .env
+    2. npm install
+    
+    For css open new terminal and type:
+    1. cd src/lib/styles
+    2. sass --watch style.scss:./style.css
+```
---
+++ map/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="main">
+    <div class="container-fluid m-0 p-0 d-flex">
+        <div class="navbars bg-dark text-white vh-100">
+            <ul>
+                <li><i class="fa fa-bars"></i>HOME</li>
+                <li><i class="fa fa-map-marker"></i>Location</li>
+                <li><i class="fa fa-phone"></i>Contact Us</li>
+                <li><i class="fa fa-envelope"></i>E-mail</li>
+            </ul>
+        </div>
+        <div class="dashboardContainer w-100 p-0 m-0">
+            <div class="dashboardHeader d-flex bg- justify-content-between ">
+                <div class="logo" >
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png" alt="logo" class="rounded-circle w-100 h-100">
+                </div>
+                <form action="#" class=>
+                    <input type="search" class="w-80 rounded border-0 ps-3" placeholder="Search">
+                    <button><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
